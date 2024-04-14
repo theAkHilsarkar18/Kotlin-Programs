3be70ee9ffb31361287e42a4c36e7bd9bba7ab72
Akhil Sodvadiya <113697861+theAkHilsarkar18@users.noreply.github.com>
2024-04-14 21:18:36 +0530

    Create variable.kt

---
+++ variable.kt
@@ -0,0 +1,9 @@
+// Create a variable to store a person's name and age, and print out a message with both details.
+
+fun main() {
+    var name : String = "Akhil Sarkar"
+    var age : Int = 22
+    
+    println("Name : " + name)
+    println("Age : " + age)
+}
