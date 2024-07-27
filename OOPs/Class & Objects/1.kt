1e69cb243b0ec5f1fe17946eec0498ddad787b31
akhil <akhilsodvadiya7@gmail.com>
2024-07-27 10:45:37 +0530

    ...

--- OOPs/Class & Objects/1.kt
+++ OOPs/Class & Objects/1.kt
@@ -3,3 +3,14 @@ and a method printDetails that prints the person's details.
 Add a secondary constructor that initializes the name with
 "Unknown" if not provided. */
 
+class Person(val name : String, val age : Int) {
+    fun printDetails(){
+        println("Name : " + name)
+        println("Age : " + age)
+    }
+}
+
+fun main() {
+    var person = Person("Akhil", 22)
+    person.printDetails()
+}   
