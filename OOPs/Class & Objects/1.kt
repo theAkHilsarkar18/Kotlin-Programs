33ad6903f088619c9cbfa2ba60a5ecd28dd887da
akhil <akhilsodvadiya7@gmail.com>
2024-07-27 10:43:28 +0530

    27-07-2024

---
+++ OOPs/Class & Objects/1.kt
@@ -0,0 +1,5 @@
+/* Create a Kotlin class Person with properties name and age,
+and a method printDetails that prints the person's details.
+Add a secondary constructor that initializes the name with
+"Unknown" if not provided. */
+
